99d7cc1b8f0823a0f3114ed830efe06e5868f4e8
Guillermo Delmas <guille@iibm>
2017-10-02 20:33:21 -0300

    tp2 ej4

--- tp2/ej4_1_filtro_de_bayes.m
+++ tp2/ej4_1_filtro_de_bayes.m
@@ -1,8 +1,11 @@
 % Filtro discreto de Bayes
-belif=[1, zeros(9,1)];
+belif=[zeros(10, 1);1; zeros(9,1)];
 nu=0;
 estados=ones([10);
+
 for i=1:length(estados)
-  belif=
+  for i=1:length(estados)
+    belif(i)=
+  endfor
   
 endfor
