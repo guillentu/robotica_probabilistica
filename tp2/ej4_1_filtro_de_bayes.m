8146f14efdc275418544b454d6a9c535c39148e8
Guillermo Delmas <guille@iibm>
2017-10-06 21:06:48 -0300

    ejercicio 4: Filtro Discreto, avance

--- tp2/ej4_1_filtro_de_bayes.m
+++ tp2/ej4_1_filtro_de_bayes.m
@@ -1,11 +1,40 @@
 % Filtro discreto de Bayes
 belif=[zeros(10, 1);1; zeros(9,1)];
-nu=0;
-estados=ones([10);
 
-for i=1:length(estados)
-  for i=1:length(estados)
-    belif(i)=
-  endfor
-  
+aux=0;
+x=2;
+
+
+aux=[.25 .5 .25 zeros(1,length(belif)-3)];
+u=[];
+for i=1:length(belif)-2
+  u=[u;shift(aux,i-1)];
 endfor
+u=[u;[zeros(1,length(belif)-2) .25 .75]];
+u=[u;[zeros(1,length(belif)-1) 1]];
+
+figure;hold on;
+plot(belif,'*r');
+for i=1:15
+  belif=sum(u.*belif,1)'; % Se aplica un comando de accion
+  plot(belif);
+endfor
+
+%%% FALTA COMANDO HACIA ATRAS %%%%%%%%%%%%%%%%%%%%%%%
+
+%for posAntes=1:length(belif)
+%
+%  if posAntes==length(belif)-1
+%    aux=[zeros(1,length(belif-2)) .25 .75] % Comando de accion
+%  elseif posAntes==length(belif)
+%    aux=[zeros(1,length(belif-1)) 1] % Comando de accion
+%  else
+%    aux=shift(u,posAntes-1) % Comando de accion
+%  endif
+%  
+%  for j=1:length(belif)
+%    auxBelif+=u(x)*belif(posAntes)  % prob(x|posAntes)*bel(posAntes)
+%  endfor
+%
+%%belif(i)=
+%endfor
