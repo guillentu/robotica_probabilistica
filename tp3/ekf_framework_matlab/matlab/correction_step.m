e53aff49b4edd2f49e9d8acc23a254497d97a48e
Guillermo <gedelmas@gmail.com>
2017-10-21 16:37:12 -0300

    tp3 punto 1 maso

--- tp3/ekf_framework_matlab/matlab/correction_step.m
+++ tp3/ekf_framework_matlab/matlab/correction_step.m
@@ -16,8 +16,8 @@ function [mu, sigma] = correction_step(mu, sigma, z, l)
     expected_ranges = zeros(size(z, 2), 1);
     for i = 1:size(z, 2)
         % Todo: Implement
-        expected_ranges=[sqrt((l(i).x-mu(1))^2+(l(i).y-mu(2))^2);  ... % distancia T0-PC
-                         atan2(l(i).y-mu(2),l(i).x-mu(1))-mu(3)];
+        expected_ranges=[sqrt((l(z(i).id).x-mu(1))^2+(l(z(i).id).y-mu(2))^2)];
+                         %atan2(l(i).y-mu(2),l(i).x-mu(1))-mu(3)];
     end
 
     % Jacobian of h
@@ -26,17 +26,17 @@ function [mu, sigma] = correction_step(mu, sigma, z, l)
     % Measurements in vectorized form
     Z = zeros(size(z, 2), 1);
     for i = 1:size(z, 2)
-        H(i, :) = [ -(sqrt((l(i).x-mu(1))^2+(l(i).y-mu(2))^2))^3*2*(l(i).x-mu(1))                     -(sqrt((l(i).x-mu(1))^2+(l(i).y-mu(2))^2))^3*2*(l(i).y-mu(2))    0];
+        H(i, :) = [ -(sqrt((l(z(i).id).x-mu(1))^2+(l(z(i).id).y-mu(2))^2))^3*2*(l(z(i).id).x-mu(1))                     -(sqrt((l(z(i).id).x-mu(1))^2+(l(z(i).id).y-mu(2))^2))^3*2*(l(z(i).id).y-mu(2))    0];
                    % -(1+((l(i).y-mu(2))/(l(i).x-mu(1)))^2)^(-1) * ((l(i).y-mu(2))/(l(i).x-mu(1))^2)   -(1+((l(i).y-mu(2))/(l(i).x-mu(1)))^2)^(-1) * 1/(l(i).x-mu(1))  -1];
         Z(i) = z(i).range;
     end
-    size(H)
-    size(Z)
+    %size(H)
+    %size(Z)
     R = diag(repmat([0.5], size(z, 2), 1));
     %dbstop('line')
     S=H*sigma*H'+R;
     K =  sigma*H'*inv(S); % Todo: Implement
-    size(K)
-    mu = mu+K*(Z-expected_ranges)% Todo: Implement
+    %size(K)
+    mu = mu+K*(Z-expected_ranges);% Todo: Implement
     sigma = (eye(3)-K*H)*sigma; % Todo: Implement
 end
