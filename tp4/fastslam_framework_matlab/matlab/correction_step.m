39fc7acce2a5ee0ae09584305f96cc4700a52118
Guillermo Delmas <guille@iibm>
2017-11-08 20:14:34 -0300

    tp4 terminado NO anda

--- tp4/fastslam_framework_matlab/matlab/correction_step.m
+++ tp4/fastslam_framework_matlab/matlab/correction_step.m
@@ -34,13 +34,20 @@ for i = 1:numParticles
     if (particles(i).landmarks(l).observed == false)
 
       % TODO: Initialize its position based on the measurement and the current robot pose:
-      particles(i).landmarks(l).mu =
+      pose = particles(i).pose;
+      T = [cos(pose(3)) -sin(pose(3)) pose(1);...% TRANSFORMACIO Afin
+           sin(pose(3)) cos(pose(3))  pose(2); ...
+                0            0           1    ];
+      
+      particles(i).landmarks(l).mu = T*[z(l).range*sin(z(l).bearing);
+                                        z(l).range*cos(z(l).bearing);
+                                                     1              ];
 
       % get the Jacobian with respect to the landmark position
       [h, H] = measurement_model(particles(i), z(j));
 
       % TODO: initialize the covariance for this landmark
-      particles(i).landmarks(l).sigma =
+      particles(i).landmarks(l).sigma = inv(H)*Q_t*inv(H)';
 
       % Indicate that this landmark has been observed
       particles(i).landmarks(l).observed = true;
@@ -51,21 +58,22 @@ for i = 1:numParticles
       [expectedZ, H] = measurement_model(particles(i), z(j));
 
       % TODO: compute the measurement covariance
-      Q =
+      Q = inv(H)*particles(i).landmarks(l).sigma*inv(H)' +  Q_t;
 
       % TODO: calculate the Kalman gain
-      K =
+      K = particles(i).landmarks(l).sigma * H' * inv(Q);
 
       % TODO: compute the error between the z and expectedZ (remember to normalize the angle using the function normalize_angle())
-      diffZ = 
-
+      diffZ = [z(j).range - expectedZ(1)  ;
+               normalize_angle(z(j).bearing - expectedZ(2))];  %% VERRRRRRR
+                                           %expectedBearing = normalize_angle(atan2(landmarkY-particle.pose(2), landmarkX-particle.pose(1)) - particle.pose(3))
       % TODO: update the mean and covariance of the EKF for this landmark
-      particles(i).landmarks(l).mu =
-      particles(i).landmarks(l).sigma =
+      particles(i).landmarks(l).mu =[particles(i).landmarks(l).mu(1:2) + K * diffZ ; particles(i).landmarks(l).mu(3)];
+      particles(i).landmarks(l).sigma = (eye(size(K))- K*H)*particles(i).landmarks(l).sigma;
 
       % TODO: compute the likelihood of this observation, multiply with the former weight
       %       to account for observing several features in one time step
-      particles(i).weight = particles(i).weight *
+      particles(i).weight = particles(i).weight * (abs(2*pi*Q))^(-0.5)*exp(-.5*diffZ'*inv(Q)*diffZ);
 
     end
 
